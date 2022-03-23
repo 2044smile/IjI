b768e513ace350d68c7a4c4d6669833af4feb1ca
2044smile <2044smile@naver.com>
2022-03-23 16:44:32 +0900

    feat: 임시적으로 백엔드와의 연결만 확인 추후엔 로그인, 회원가입 목표

--- app/src/main/java/com/example/iji/JoinActivity.kt
+++ app/src/main/java/com/example/iji/JoinActivity.kt
@@ -15,59 +15,23 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class JoinActivity : AppCompatActivity() { // 회원가입 페이지 Ex) 구글, 네이버, 카카오, 회원가입
+
+    val second: String = "G, N, K, 사설 회원가입"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
         val btn = findViewById<Button>(R.id.btnJoinBasics) as Button
-
         btn.setOnClickListener { // 사용자가 버튼을 눌렀을 때 반응하는 이벤트 처리
+//            로그인 후 다른 액티비티로 전환
+            Log.d(second, "가입 버튼 클릭")
             val intent = Intent(this, JoinBasicsActivity::class.java)
-            intent.putExtra("email", "2044smile@naver.com")
-            intent.putExtra("password1", 1234)
-            intent.putExtra("password2", 1234)
             startActivity(intent)
+//            전환 된 액티비티에 데이터 값 전달 ex) Welcome to IjI
+//            intent.putExtra("email", "2044smile@naver.com")
+//            intent.putExtra("password1", 1234)
+//            intent.putExtra("password2", 1234)
         }
-
-    fun getJoinBasics() {
-        var th: Thread = object : Thread() {
-            override fun run() {
-                super.run()
-                val json = JSONObject()
-                json.put("email", edit_id)
-                json.put("password1", edit_password_1)
-                json.put("password2", edit_password_2)
-
-                val url =
-                    URL("http://ec2-3-35-54-69.ap-northeast-2.compute.amazonaws.com:3000/api/routerTest")
-                var conn: HttpURLConnection? = null
-                conn = url.openConnection() as HttpURLConnection
-                conn.doOutput = true
-                conn.requestMethod = "POST"
-                conn.setRequestProperty("Connection", "Keep-Alive")
-                conn.setRequestProperty("Content-Type", "application/json")
-
-                val jsonStr = json.toString() // json을 string으로 변환 후 서버로 전송
-                val os: OutputStream = conn.outputStream
-                os.write(jsonStr.toByteArray(charset("UTF-8")))
-                os.flush()
-
-                val sb = StringBuilder()
-                val httpResult = conn.responseCode
-                if (httpResult == HttpURLConnection.HTTP_OK) {
-                    val br = BufferedReader(
-                        InputStreamReader(conn.inputStream, "utf-8")
-                    )
-
-                    br.close()
-                    println("" + sb.toString())
-                } else
-                    println(conn.responseMessage)
-                os.close()
-                Log.d("json", "" + jsonStr)
-            }
-        }
-        th.start()
     }
-}
 }
--- app/src/main/java/com/example/iji/JoinBasicsActivity.kt
+++ app/src/main/java/com/example/iji/JoinBasicsActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.example.iji.retrofit.RetrofitManager
+import com.example.iji.utils.Constants.TAG
+import com.example.iji.utils.RESPONSE_STATE
 import kotlinx.android.synthetic.main.activity_join.*
 import kotlinx.android.synthetic.main.activity_join_basics.*
 import org.json.JSONObject
@@ -14,59 +18,62 @@ import java.io.OutputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
+
 class JoinBasicsActivity : AppCompatActivity() {
+
+    val third: String = "사설 가입 - 회원가입"
+    var isExistBlank = false
+    var isPWSame = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_basics)
 
         val btnJoinMembership = findViewById<Button>(R.id.btnLogin_S) as Button
 
-        btnJoinMembership.setOnClickListener{
-            val intent = Intent(this, HomeActivity::class.java)
+        btnJoinMembership.setOnClickListener {
+            Log.d(third, "버튼 클릭 ")
+            var json = JSONObject()
+            val email = edit_id.text.toString()
+            val password1 = edit_password_1.text.toString()
+            val password2 = edit_password_2.text.toString()
+
+            if (email.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
+                isExistBlank = true
+                Log.d(third, "이메일, 패스워드1, 패스워드2 실패")
+            } else {
+                if(password1 == password2){
+                    isPWSame = true
+                } else {
+                    Log.d(third, "조건문 패스워드 비교 실패")
+                }
+            }
+
+            if(!isExistBlank && isPWSame) {
+                Log.d(third, "메인 진입")
+
+                // 백엔드와 통신이 이루어져야한다.
+                // 사전적으로 email 과 P1 P2는 검사를 했기에
+                RetrofitManager.instance.apiTest(term = null, completion = {
+                    responseState, responseBody ->
+
+                    when(responseState) {
+                        RESPONSE_STATE.OKAY -> {
+                            Log.d(TAG, "API 호출 성공 : $responseBody")
+                        }
+                        RESPONSE_STATE.FAIL -> {
+                            // Toast 임시로 띄워준다 LENGTH_SHORT를 하면 짧게 화면에 노출
+                            Toast.makeText(this, "API 호출 에러입니다.", Toast.LENGTH_SHORT).show()
+                            Log.d(TAG, "API 호출 실패 : $responseBody")
+                        }
+                    }
+                })
+
+                // 홈 화면으로 이동한다.
+                val intent = Intent(this, HomeActivity::class.java)
+                Log.d(third, "홈 버튼 클릭")
                 startActivity(intent)
             }
         }
     }
-//
-//    private fun getJoinBasics() {
-//
-//        var th: Thread = object : Thread() {
-//            override fun run() {
-//                super.run()
-//                val json = JSONObject()
-//                json.put("email", edit_id)
-//                json.put("password1", edit_password_1)
-//                json.put("password2", edit_password_2)
-//
-//                val url = URL("http://ec2-3-35-54-69.ap-northeast-2.compute.amazonaws.com:3000/api/routerTest")
-//                var conn: HttpURLConnection? = null
-//                conn = url.openConnection() as HttpURLConnection
-//                conn.doOutput = true
-//                conn.requestMethod = "POST"
-//                conn.setRequestProperty("Connection", "Keep-Alive")
-//                conn.setRequestProperty("Content-Type", "application/json")
-//
-//                val jsonStr = json.toString() // json을 string으로 변환 후 서버로 전송
-//                val os: OutputStream = conn.outputStream
-//                os.write(jsonStr.toByteArray(charset("UTF-8")))
-//                os.flush()
-//
-//                val sb = StringBuilder()
-//                val httpResult = conn.responseCode
-//                if (httpResult == HttpURLConnection.HTTP_OK) {
-//                    val br = BufferedReader(
-//                        InputStreamReader(conn.inputStream, "utf-8")
-//                    )
-//
-//                    br.close()
-//                    println("" + sb.toString())
-//                } else
-//                    println(conn.responseMessage)
-//                os.close()
-//                Log.d("json", "" + jsonStr)
-//            }
-//        }
-//    th.start()
-//    }
-//  }
-// }
+}
