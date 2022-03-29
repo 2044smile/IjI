6767ecdf57433fd1a1724f63b469c33bd805a8c4
2044smile <2044smile@naver.com>
2022-03-30 07:30:43 +0900

    modify: 주석 추가

--- app/src/main/java/com/example/iji/utils/Constants.kt
+++ app/src/main/java/com/example/iji/utils/Constants.kt
@@ -4,17 +4,12 @@ object Constants {
     const val TAG: String = "로그"
 }
 
-enum class SEARCH_TYPE {
-    PHOTO,
-    USER
-}
-
 enum class RESPONSE_STATE {
     OKAY,
     FAIL
 }
 
-object API {
+object API { // 변수에 값을 박아놓은 느낌?
     const val BASE_URL : String = "http://ec2-3-35-54-69.ap-northeast-2.compute.amazonaws.com:3000/"
     const val TEST : String = "api/routerTest"
 }
