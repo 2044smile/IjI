dcab8ac0bc3c05845280cace4462983a89e4fbe1
2044smile <2044smile@naver.com>
2022-03-31 17:16:43 +0900

    feat: 남자, 여자 Button 클릭 프론트 완성 / 백엔드 구현 필요

--- app/src/main/java/com/example/iji/JoinBasicsActivity.kt
+++ app/src/main/java/com/example/iji/JoinBasicsActivity.kt
@@ -5,8 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.Toast
 import com.example.iji.retrofit.RetrofitManager
 import com.example.iji.utils.Constants.TAG
 import com.example.iji.utils.RESPONSE_STATE
@@ -14,7 +12,9 @@ import kotlinx.android.synthetic.main.activity_join.*
 import kotlinx.android.synthetic.main.activity_join_basics.*
 import org.json.JSONObject
 import android.util.Patterns
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import com.example.iji.databinding.ActivityJoinBasicsBinding
 import java.util.*
 import java.util.regex.Pattern
 
@@ -25,15 +25,41 @@ class JoinBasicsActivity : AppCompatActivity() {
     var isExistBlank = false
     var isPWSame = false
     var dateString = ""
-    var timeString = ""
+
+    fun onRadioButtonClicked(view: View) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_basics)
         val btnDateOfBirth = findViewById<TextView>(R.id.edit_dateOfBirth) as TextView
         val btnJoinMembership = findViewById<Button>(R.id.btnLogin_S) as Button
+//
+//        val spinner: Spinner = findViewById(R.id.edit_gender)
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.genderList,
+//            R.layout.activity_join_basics
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(R.layout.activity_join_basics)
+//            spinner.adapter=adapter
+//        }
 
-        btnDateOfBirth.setOnClickListener {
+//        var gender = resources.getStringArray(R.array.genderList)
+        // 47line 까지 에러가 발생
+//        var adapter = ArrayAdapter<String>(this, R.layout.activity_join_basics, R.id.edit_gender, gender)
+//        spinner.adapter = adapter
+//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                Log.d(third, "start")
+//            }
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                Log.d(third, "stop")
+//            }
+//        }
+
+        btnDateOfBirth.setOnClickListener { // 생년월일
             val cal = Calendar.getInstance()
             val dateSetListener = DatePickerDialog.OnDateSetListener {
                 view, year, month, dayOfMonth ->
@@ -44,7 +70,7 @@ class JoinBasicsActivity : AppCompatActivity() {
             cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
         }
 
-        btnJoinMembership.setOnClickListener {
+        btnJoinMembership.setOnClickListener { // ID, Password
             Log.d(third, "버튼 클릭 ")
             val email = edit_id.text.toString()
             val password1 = edit_password_1.text.toString()
