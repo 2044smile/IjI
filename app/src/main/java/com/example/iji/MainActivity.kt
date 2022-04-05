b71a1d8e73d1a5a7f987e52e41073c2646935259
2044smile <2044smile@naver.com>
2022-04-05 19:07:57 +0900

    fix: MainActivity에서 btnLogin 생성 / 로그인 페이지

--- app/src/main/java/com/example/iji/MainActivity.kt
+++ app/src/main/java/com/example/iji/MainActivity.kt
@@ -12,9 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.btnJoin) as Button // 첫 번째 회원가입을 누르면 JoinActivity
+        val btnLogin = findViewById<Button>(R.id.btnLogin) as Button // 로그인을 누르면
 
         btn.setOnClickListener{
             val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
