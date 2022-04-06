7a2096d7c694052493ba88c63cbac8ccb51b2f6d
2044smile <2044smile@naver.com>
2022-04-07 07:36:33 +0900

    test: testAPI

--- app/src/main/java/com/example/iji/retrofit/RetrofitManager.kt
+++ app/src/main/java/com/example/iji/retrofit/RetrofitManager.kt
@@ -1,16 +1,14 @@
 package com.example.iji.retrofit
 
 import android.util.Log
-import retrofit2.Retrofit
 import com.example.iji.utils.API
-import com.example.iji.retrofit.IRetrofit
 import com.example.iji.utils.Constants.TAG
 import com.example.iji.utils.RESPONSE_STATE
 import com.google.gson.JsonElement
 import retrofit2.Call
 import retrofit2.Response
 
-class RetrofitManager {
+class RetrofitManager { // 인스턴스 생성
 
     companion object { // 싱글턴이 적용되도록
         val instance = RetrofitManager() // RetrofitManager의 인스턴스만 가져온다.
@@ -24,14 +22,14 @@ class RetrofitManager {
         val t = term.let {
             it
         }?: ""
-        val call: Call<JsonElement> = iRetrofit?.apiTest(term=t) ?: return
-        // Q) 아래의 작업이 오래걸린다, 화면 이동은 홈으로 이동되었으나 RetrofitManager 로그는 5초 뒤에 온다.
-        call.enqueue(object: retrofit2.Callback<JsonElement>{
-            // 웹 통신 응답 실패 시
-            override fun onFailure(call: Call<JsonElement>, t: Throwable) {
-                Log.d(TAG, "RetrofitManager - onFailure() called / t: $t")
+        val call: Call<JsonElement> = (iRetrofit?.login(email =t, password =t) ?: return) as Call<JsonElement>
+                // Q) 아래의 작업이 오래걸린다, 화면 이동은 홈으로 이동되었으나 RetrofitManager 로그는 5초 뒤에 온다.
+                call.enqueue(object: retrofit2.Callback<JsonElement>{
+                    // 웹 통신 응답 실패 시
+                    override fun onFailure(call: Call<JsonElement>, t: Throwable) {
+                        Log.d(TAG, "RetrofitManager - onFailure() called / t: $t")
 
-                completion(RESPONSE_STATE.FAIL, t.toString())
+                        completion(RESPONSE_STATE.FAIL, t.toString())
             }
             // 웹 통신 응답 성공 시
             override fun onResponse(call: Call<JsonElement>, response: Response<JsonElement>) {
