1f14ff03883f05ddc7a7a6f6604fa9393fc38c04
2044smile <2044smile@naver.com>
2022-04-08 08:16:16 +0900

    feat: data class created

---
+++ app/src/main/java/com/example/iji/models/DefaultResponse.kt
@@ -0,0 +1,3 @@
+package com.example.iji.models
+
+data class DefaultResponse(val error: Boolean, val message: String)
---
+++ app/src/main/java/com/example/iji/models/LoginResponse.kt
@@ -0,0 +1,3 @@
+package com.example.iji.models
+
+data class LoginResponse(val email: String, val password: String)
