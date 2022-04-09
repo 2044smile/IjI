2c8dc70a1831c68fd038c5413d2b06ae5c2170b5
2044smile <2044smile@naver.com>
2022-04-09 20:27:48 +0900

    annotation: 정리

--- app/src/main/java/com/example/iji/api/Api.kt
+++ app/src/main/java/com/example/iji/api/Api.kt
@@ -13,6 +13,13 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface Api {
+    // 내용 정리
+    // Retrofit, 송신 시 우리는 값을 디코딩한 상태로 보였지만, OkHttpClient 에서는 인코딩 된 상태로 확인되었고,
+    // 그게 백엔드로 전송이되면 당연히 오류가 발생했고, 인코딩 문제를 해결하려고 하였음
+    // 최종 정리
+    // postman 으로도 원하는 값을 넣어 보내도 오류가 발생하는 문제 확인완료.
+    // OkHttpClient 는 @FormUrlEncoded 로 인코딩이 된 걸로 확인(확실하지 않음) 그래서 백엔드에서 인코딩 된 걸 디코딩하여
+    // 받아야 되는 걸로 확인 - help 조이스
     @FormUrlEncoded
     @POST("/api/auth/signup/")
     fun createUser(
