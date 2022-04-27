920da09ff59485d506dcbb1756d227d74ef73785
2044smile <2044smile@naver.com>
2022-04-28 08:01:12 +0900

    feat: 로그인이 되어 있다면 로그인을 볼 필요가 없기에 GONE으로 처리하였음

--- app/src/main/java/com/example/iji/MainActivity.kt
+++ app/src/main/java/com/example/iji/MainActivity.kt
@@ -9,32 +9,13 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.example.iji.api.MyApplication
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.option_menu, menu)
-//
-//        menu?.add(Menu.NONE, Menu.FIRST + 1, Menu.NONE, "LOGIN")
-//        menu?.add(Menu.NONE, Menu.FIRST + 2, Menu.NONE, "LOGOUT")
-//
-//        return super.onCreateOptionsMenu(menu)
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId){
-//            R.id.menu_login ->
-//                btnLogin.text = "LOGIN"
-//            R.id.menu_logout ->
-//                btnLogout.text = "LOGOUT"
-//
-//            Menu.FIRST + 1 -> btnLogin.text = "LOGIN CLICK"
-//            Menu.FIRST + 2 -> btnLogout.text = "LOGOUT CLICK"
-//        }
-//
-//        return super.onOptionsItemSelected(item)
-//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,12 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         if (email == "" && password == ""){
             val btnLogin = findViewById<Button>(R.id.btnLogin) as Button
+            btnLogout.visibility = View.GONE
             btnLogin.setOnClickListener{
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
             }
         } else {
             val btnLogout = findViewById<Button>(R.id.btnLogout) as Button
+            btnLogin.visibility = View.GONE
             btnLogout.setOnClickListener{
                 val intent = Intent(this, LogoutActivity::class.java)
                 startActivity(intent)
