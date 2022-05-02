f59b0661e8ad52585af973936e318374de5297d6
2044smile <2044smile@naver.com>
2022-05-02 21:53:29 +0900

    refactor: think

--- app/src/main/java/com/example/iji/HomeActivity.kt
+++ app/src/main/java/com/example/iji/HomeActivity.kt
@@ -1,26 +1,35 @@
 package com.example.iji
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_join_basics.*
-import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.OutputStream
-import java.net.HttpURLConnection
-import java.net.URL
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_home.*
+import me.relex.circleindicator.CircleIndicator3
 
 class HomeActivity : AppCompatActivity() { // 메인(홈) 페이지
     // 1. ViewPager2 확정
+
+    private var imagesList = mutableListOf<Int>() // mutable 변경가능한
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        postToList()
 
+        view_pager2.adapter = ViewPagerAdapter(imagesList)
+        view_pager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
+        val indicator = findViewById<CircleIndicator3>(R.id.indicator)
+        indicator.setViewPager(view_pager2)
+    }
+
+    private fun addToList(image: Int){
+        imagesList.add(image)
+    }
+    private fun postToList() {
+        for (i in 1..5){
+            addToList(R.mipmap.ic_launcher_round)
+        }
     }
 }
