dad2f79a7bba3bcc376306abd17a80cfd9db171f
2044smile <2044smile@naver.com>
2022-05-06 23:51:45 +0900

    comment: Home Activity Main, ViewPager2 Ad service

--- app/src/main/java/com/example/iji/HomeActivity.kt
+++ app/src/main/java/com/example/iji/HomeActivity.kt
@@ -7,4 +7,5 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() { // 메인(홈) 페이지
     // 1. ViewPager2 확정
+    // Home Activity 를 메인으로 두고, Viewpager2 로 광고 서비스 도입
 }
